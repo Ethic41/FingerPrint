4235fb7322f2d7731ff1ade9f20b269125592d16
ethic41 <dahirmuhammad3@gmail.com>
2022-08-10 23:02:50 +0100

    update: configs.php

--- configs.php
+++ configs.php
@@ -1,2 +1,7 @@
 <?php
+/**
+ * this should be the address of the fingerprint client
+ * service
+ * i.e the service for this repo: https://github.com/Ethic41/php-grpc-fingerprint-engine-client.git
+ */
 putenv("FP_CLIENT_SERVICE_HOST=http://localhost:5555");
